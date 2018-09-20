35c9f01ead2483ffcd313bbfda585bdfb55a600e
parth <parthematics@gmail.com>
2018-09-19 20:00:59 -0700

    made more readable

--- test_matcher.py
+++ test_matcher.py
@@ -1,11 +1,8 @@
 from pair import Pairer
 
 littles = dict((l, prefs.split(', ')) for [l, prefs] in (line.rstrip().split(': ') for line in open('littles.txt')))
-
 bigs = dict((b, prefs.split(', ')) for [b, prefs] in (line.rstrip().split(': ') for line in open('bigs.txt')))
-
 paired = Pairer(littles, bigs)
-
 pairings = paired.pair()
 
 for key in pairings.keys():
